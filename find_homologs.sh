251f16e14a724fb6b4471effe2b21e25d386b794
yonatan <yonatanamsalu1234@gmail.com>
2026-09-21 10:04:50 -0400

    added the filtering

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,7 +2,8 @@
 query=$1
 input_file=$2
 outfile=$3
-
 tblastn -query $1 -subject $2 -outfmt "6 qseqid sseqid pident qcovs" -out $3
-
+#awk filtering
+awk '$3 > 30 && $4 > 90' $3 > tmp.txt
+mv tmp.txt $3
 wc -l $3
