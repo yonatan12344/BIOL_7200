d03c870964961334a0306979000ed6578eafc3d5
yonatan <yonatanamsalu1234@gmail.com>
2026-09-21 09:22:22 -0400

    Had to modify blast command a little bit to fix things

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,6 +3,6 @@ query=$1
 input_file=$2
 outfile=$3
 
-tblastn -query $1 -subject $2 -outfmt 6 "qseqid sseqid pident qcovs" -out $3
+tblastn -query $1 -subject $2 -outfmt "6 qseqid sseqid pident qcovs" -out $3
 
 wc -l $3
